8138e03e6da406cbd8a4405d81b8ea40866adcf7
Kairo0628 <112845598+Kairo0628@users.noreply.github.com>
2026-02-05 15:36:23 +0900

    Software: Docstring

---
+++ README.md
@@ -0,0 +1,3 @@
+# TIL
+
+Today I Learned, 오늘 배운 것
---
+++ Software, Testing/docstring.py
@@ -0,0 +1,25 @@
+"""
+모듈 최상단에 작성된 설명 글입니다.
+"""
+
+class MyClass:
+    """
+    이것은 클래스의 독스트링입니다.
+    """
+
+    def __init__(self):
+        """
+        MyClass 객체를 생성할 때 동작하는 함수입니다.
+        """
+
+    def my_class_method(self):
+        """
+        my_class_method에 대한 설명입니다.
+        """
+
+def my_custom_method(x, y):
+    """
+    my_custom_method에 대한 설명입니다.
+    두 값을 인자로 받아 더한 값을 반환합니다.
+    """
+    return x + y
